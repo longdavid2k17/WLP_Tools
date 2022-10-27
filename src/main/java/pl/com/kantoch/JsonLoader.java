7cc42bffd443136dfddfcc70df1f6ea54fa6b241
Dawid <kantochdawid98@gmail.com>
2022-10-27 11:54:22 +0200

    Providing new definition of loading file

--- src/main/java/pl/com/kantoch/JsonLoader.java
+++ src/main/java/pl/com/kantoch/JsonLoader.java
@@ -3,14 +3,22 @@ package pl.com.kantoch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.stream.Collectors;
 
 public class JsonLoader
 {
     public String loadFile(String filePath) throws Exception {
+        File file = new File(filePath);
+        if(!file.exists()) return loadFileFromResource(filePath);
+        Object object;
+        InputStream inputStream = new FileInputStream(file);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        object = (String)bufferedReader.lines().collect(Collectors.joining("\n"));
+        return (String) object;
+    }
+
+    public String loadFileFromResource(String filePath) throws Exception {
         Object object;
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
         if(inputStream==null) throw new Exception("InputStream instance is null.");
